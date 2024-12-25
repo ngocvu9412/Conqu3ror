2cffd6cdf90583f0bc95689eec60f1267e11e5a3
ntdsam2830 <ntdsam@gmail.com>
2024-12-25 15:13:15 +0700

    delete debug log

--- Assets/Scripts/KnightMovement.cs
+++ Assets/Scripts/KnightMovement.cs
@@ -91,8 +91,6 @@ public class KnightMovement : MonoBehaviour
         }
 
         Animate();
-        Debug.Log(moveDirection.x);
-        Debug.Log(moveDirection.y);
     }
 
     private void Animate()
