a4fbf86c141dd406152ff9d748fb1502a52379a3
ntdsam2830 <ntdsam@gmail.com>
2024-12-25 13:41:49 +0700

    Prevent moving when opening dialog

--- Assets/Scripts/DialogController.cs
+++ Assets/Scripts/DialogController.cs
@@ -9,6 +9,12 @@ public class DialogController : MonoBehaviour
     public UnityEngine.UI.Image textboxImage2;
     public UnityEngine.UI.Image menuImage;
     public UnityEngine.UI.Button menuBtn;
+
+    public KnightMovement character;
+
+    public bool isOpened = false;
+
+
     void Update()
     {
         // Kiểm tra nếu nhấn chuột trái
@@ -25,7 +31,7 @@ public class DialogController : MonoBehaviour
                 // Hiển thị dialog
                 if (hit.collider.gameObject.CompareTag("RedPoint"))
                 {
-                    textboxImage1.gameObject.SetActive(true);
+                    ShowTextBox();
                 }
             }
         }
@@ -36,34 +42,51 @@ public class DialogController : MonoBehaviour
     {
         HideTextbox(textboxImage2);
         dialogImage.gameObject.SetActive(true);
+        isOpened = true;
     }
 
     // Gọi hàm này để ẩn dialog (nút Close hoặc sau khi hoàn thành hành động)
     public void HideDialog()
     {
         dialogImage.gameObject.SetActive(false);
-    }
+        isOpened = false;
 
+    }
+    public void ShowTextBox()
+    {
+        textboxImage1.gameObject.SetActive(true);
+        isOpened = true;
+
+    }
     public void ShowNextTextbox()
     {
         HideTextbox(textboxImage1);
         textboxImage2.gameObject.SetActive(true);
+        isOpened = true;
+
+
     }
 
     public void HideTextbox(UnityEngine.UI.Image textbox)
     {
         textbox.gameObject.SetActive(false);
+        isOpened = false;
+
     }
 
     public void OpenMenu()
     {
         menuImage.gameObject.SetActive(true);
         menuBtn.interactable = false;
+        isOpened = true;
+
     }
     public void HideMenu()
     {
         menuImage.gameObject.SetActive(false);
         menuBtn.interactable = true;
+        isOpened = false;
+
     }
     public void PlayGame()
     {
--- Assets/Scripts/KnightMovement.cs
+++ Assets/Scripts/KnightMovement.cs
@@ -1,82 +1,119 @@
-using System;
+﻿using System;
 using UnityEngine;
 
 
 public class KnightMovement : MonoBehaviour
 {
-	public float speed = 10f;
-	private Vector2 lastClickedPos;
-	private Vector2 moveDirection;
-	private bool moving;
-	public static bool isShip;
-	public static bool isUIClick = false;
-	private Animator animator;
+    public float speed = 10f;
+    private Vector2 lastClickedPos;
+    private Vector2 moveDirection;
+    public bool isMoving = false;
+    public bool isActive = true;
+    public static bool isShip;
+    public static bool isUIClick = false;
+    private Animator animator;
 
-	private void Start()
-	{
-		//animator = GetComponent<Animator>();
-	}
+    public DialogController dialogManager; // Tham chiếu đến DialogManager
 
-	private void LateUpdate()
-	{
-		float moveDirX = 0;
-		float moveDirY = 0;
+    private void Start()
+    {
+        //animator = GetComponent<Animator>();
+    }
 
-		if (Input.GetMouseButtonDown(0) && !isUIClick)
-		{
-			lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			moving = true;
+    private void LateUpdate()
+    {
+        if (dialogManager.isOpened)
+        {
+            isActive = false;
+            return;
+        }
 
-			if (lastClickedPos.x > transform.position.x)
-			{
-				moveDirX = 1;
-			}
-			else if (lastClickedPos.x < transform.position.x)
-			{
-				moveDirX = -1;
-			}
+        float moveDirX = 0;
+        float moveDirY = 0;
 
-			if (Math.Abs(lastClickedPos.y - transform.position.y) <
-				Math.Abs(lastClickedPos.x - transform.position.x))
-			{
-				moveDirY = 0;
-			}
-			else if (lastClickedPos.y > transform.position.y)
-			{
-				moveDirY = 1;
-			}
-			else if (lastClickedPos.y < transform.position.y)
-			{
-				moveDirY = -1;
-			}
+        if (Input.GetMouseButtonDown(0) && !isUIClick)
+        {
+            lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            isMoving = true;
 
-			moveDirection = new Vector2(moveDirX, moveDirY);
-		}
-		else
-		{
-			isUIClick = false;
-		}
+            if (lastClickedPos.x > transform.position.x)
+            {
+                moveDirX = 1;
+            }
+            else if (lastClickedPos.x < transform.position.x)
+            {
+                moveDirX = -1;
+            }
 
-		if (moving && (Vector2)transform.position != lastClickedPos)
-		{
-			var step = speed * Time.deltaTime;
-			Vector3 newPosition = new(lastClickedPos.x, lastClickedPos.y, transform.position.z);
-			transform.position = Vector3.MoveTowards(transform.position, newPosition, step);
-		}
-		else
-		{
-			moving = false;
-		}
+            if (Math.Abs(lastClickedPos.y - transform.position.y) <
+                Math.Abs(lastClickedPos.x - transform.position.x))
+            {
+                moveDirY = 0;
+            }
+            else if (lastClickedPos.y > transform.position.y)
+            {
+                moveDirY = 1;
+            }
+            else if (lastClickedPos.y < transform.position.y)
+            {
+                moveDirY = -1;
+            }
 
-		//Animate();
-	}
+            moveDirection = new Vector2(moveDirX, moveDirY);
+        }
+        else
+        {
+            isUIClick = false;
+        }
 
-	private void Animate()
-	{
-		animator.SetBool("IsMoving", moving);
-		animator.SetFloat("LastMoveX", moveDirection.x);
-		animator.SetFloat("LastMoveY", moveDirection.y);
-		animator.SetBool("IsShip", isShip);
-	}
+        if (isMoving && (Vector2)transform.position != lastClickedPos)
+        {
+            var step = speed * Time.deltaTime;
+            Vector3 newPosition = new(lastClickedPos.x, lastClickedPos.y, transform.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, newPosition, step);
+
+            //if (Vector2.Distance(transform.position, lastClickedPos) < 0.1f)
+            //{
+            //    isMoving = false;
+
+            //    // Kiểm tra nếu chạm đối tượng có tag "Red Point"
+            //    Collider2D hit = Physics2D.OverlapCircle(transform.position, 0.1f);
+            //    if (hit != null && hit.CompareTag("RedPoint"))
+            //    {
+            //        dialogManager.ShowTextBox(); // Gọi sự kiện khi đến Red Point
+            //    }
+            //}
+        }
+        else
+        {
+            isMoving = false;
+        }
+
+        //Animate();
+
+
+
+    }
+
+    private void Animate()
+    {
+        animator.SetBool("IsMoving", isMoving);
+        animator.SetFloat("LastMoveX", moveDirection.x);
+        animator.SetFloat("LastMoveY", moveDirection.y);
+        animator.SetBool("IsShip", isShip);
+    }
+
+    public void EnableCharacter()
+    {
+        isActive = true; // Kích hoạt nhân vật
+    }
+
+    public void DisableCharacter()
+    {
+        isActive = false; // Vô hiệu hóa nhân vật
+        isMoving = false; // Ngừng mọi chuyển động
+    }
+
+
 }
 
