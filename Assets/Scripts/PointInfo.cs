239a82edff7eb058fc17560427075f7be0af260a
TanPhuc1805 <147247353+TanPhuc1805@users.noreply.github.com>
2024-12-26 00:00:18 +0700

    .

--- Assets/Scripts/PointInfo.cs
+++ Assets/Scripts/PointInfo.cs
@@ -1,6 +1,19 @@
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class PointInfo
 {
-    
+    public string PointName;
+    public bool status; //red or green
+
+    //Enemy
+    public Sprite Image;
+    public string EnemyName;
+    public int EnemyHealth;
+    public int EnemyAttack;
+    public int EnemyLevel;
+    public List<Skill> Skills; // Danh sách kỹ năng của kẻ địch
+
+    public List<(Sprite, string, string)> conversationBefore; //ảnh, tên nhân vật, hội thoại
+    public List<(Sprite, string, string, bool)> conversationAfter; //ảnh, tên nhân vật, hội thoại, win/lose
 }
