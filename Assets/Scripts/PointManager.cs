eb80b75575b14751add3aa71fa3d44026766c4f2
ntdsam2830 <ntdsam@gmail.com>
2024-12-26 13:07:39 +0700

    add logic change point

---
+++ Assets/Scripts/PointManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PointManager : MonoBehaviour
+{
+    public GameObject redPoint; // Asset điểm đỏ
+    public int pointIndex;
+    public PointInfosDatabase pointInfo; // Thông tin trạng thái của điểm
+
+    private void Start()
+    {
+        UpdatePointState(); // Kiểm tra và cập nhật trạng thái khi bắt đầu
+    }
+
+    public void UpdatePointState()
+    {
+        if (pointInfo.GetPointInfos(pointIndex).status) // Nếu đã thắng
+        {
+            redPoint.SetActive(false); // Ẩn Red Point
+        }
+    }
+
+    public void SetPointState()
+    {
+        pointInfo.GetPointInfos(pointIndex).status = true; // Cập nhật trạng thái trong PointInfo
+        UpdatePointState(); // Gọi lại hàm để cập nhật hiển thị
+    }
+}
